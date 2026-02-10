798e8991c28805ae1e6358f193586c76581032bc
MuhammadBrataH <muhammadbrata06@gmail.com>
2026-02-10 11:02:29 +0700

    TASK 1 FINAL

--- lib/counter_view.dart
+++ lib/counter_view.dart
@@ -18,6 +18,30 @@ class _CounterViewState extends State<CounterView> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Text("Total Hitungan:", style: TextStyle( fontSize: 18)),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton.tonal(
+                  onPressed: () => setState(() => _controller.decrement()),
+                  child: const Icon(Icons.remove),
+                  style: FilledButton.styleFrom(backgroundColor: Colors.redAccent),
+                ),
+                const SizedBox(width: 15),
+                Text(
+                  '${_controller.value}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(width: 15),
+                FilledButton.tonal(
+                  onPressed: () => setState(() => _controller.increment()),
+                  child: const Icon(Icons.add),
+                  style: FilledButton.styleFrom(backgroundColor: Colors.greenAccent),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
             Slider(
               value: _controller.step.toDouble(),
               min: 1,
@@ -30,14 +54,14 @@ class _CounterViewState extends State<CounterView> {
                 });
               },
             ),
-            const Text("Total Hitungan:"),
-            Text('${_controller.value}', style: const TextStyle(fontSize: 40)),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+              onPressed: () => setState(() =>  _controller.reset()),
+              child: const Text("Reset Counter"),
+              style: FilledButton.styleFrom(backgroundColor: Colors.grey),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _controller.increment()),
-        child: const Icon(Icons.add),
       ),
     );
   }
